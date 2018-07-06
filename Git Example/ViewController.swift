f1d30e648637de9867f3604da32ae31176283f64
Tom Bredemeier <tbredemeier@barrington220.org>
2018-07-06 09:57:39 -0500

    Initial Commit

---
+++ Git Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git Example
+//
+//  Created by tbredemeier on 7/6/18.
+//  Copyright © 2018 tbredemeier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
